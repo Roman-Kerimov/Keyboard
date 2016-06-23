d4d25682b87d5fd946faf82d1ce7b9b60b809581
Roman Kerimov <roma.kerimov@icloud.com>
2016-06-23 03:32:36 +0300

    Added iOSKeyboardApplication and iOSKeyboardExtension targets.

---
+++ iOSKeyboardApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSKeyboardApplication
+//
+//  Created by Roman Kerimov on 23.06.16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

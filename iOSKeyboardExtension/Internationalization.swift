f9bf2cee373758ccef09d56f94ed082f596b869b
Roman Kerimov <roma.kerimov@icloud.com>
2017-01-15 02:43:31 +0300

    Added languages.

--- iOSKeyboardExtension/Internationalization.swift
+++ iOSKeyboardExtension/Internationalization.swift
@@ -9,22 +9,28 @@
 import Foundation
 
 protocol InternationalizedString {
+    static var cs: String {get}
     static var de: String {get}
     static var en: String {get}
     static var es: String {get}
     static var fr: String {get}
     static var it: String {get}
+    static var nl: String {get}
+    static var pl: String {get}
     static var pt: String {get}
     static var ru: String {get}
     static var sv: String {get}
 }
 
 enum Language: String {
+    case cs = "cs"
     case de = "de"
     case en = "en"
     case es = "es"
     case fr = "fr"
     case it = "it"
+    case nl = "nl"
+    case pl = "pl"
     case pt = "pt"
     case ru = "ru"
     case sv = "sv"
@@ -34,29 +40,17 @@ extension InternationalizedString {
     static var string: String {
         switch Internationalize.language {
             
-        case .de:
-            return de
-            
-        case .en:
-            return en
-            
-        case .es:
-            return es
-            
-        case .fr:
-            return fr
-            
-        case .it:
-            return it
-            
-        case .pt:
-            return pt
-            
-        case .ru:
-            return ru
-            
-        case .sv:
-            return sv
+        case .cs: return cs
+        case .de: return de
+        case .en: return en
+        case .es: return es
+        case .fr: return fr
+        case .it: return it
+        case .nl: return nl
+        case .pl: return pl
+        case .pt: return pt
+        case .ru: return ru
+        case .sv: return sv
             
         }
     }

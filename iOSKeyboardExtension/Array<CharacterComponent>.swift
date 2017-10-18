da1dfb6acd3ff5071c75953c713a417fd0286a85
Roman Kerimov <roma.kerimov@icloud.com>
2017-10-18 14:10:07 +0300

    Add convertation from character components to character

--- iOSKeyboardExtension/Array<CharacterComponent>.swift
+++ iOSKeyboardExtension/Array<CharacterComponent>.swift
@@ -8,7 +8,17 @@
 import Foundation
 
 extension Array where Element == CharacterComponent {
+    
+    private static let characterDictionary: [String: String] = .init(
+        uniqueKeysWithValues: characterComponentsDictionary.filter {!$0.value.isEmpty} .map {($0.value.key, $0.key)}
+    )
+    
     var key: String {
         return self.map {$0.hashValue.description}.joined(separator: String.space)
     }
+    
+    var character: String {
+        return type(of: self).characterDictionary[self.key] ?? .init()
+    }
+
 }

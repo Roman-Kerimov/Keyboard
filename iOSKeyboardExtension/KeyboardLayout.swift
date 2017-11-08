f3325640e443648a5ad0ca0747d7c76ea602d237
Roman Kerimov <roma.kerimov@icloud.com>
2017-11-08 22:40:12 +0300

    Refactor

--- iOSKeyboardExtension/KeyboardLayout.swift
+++ iOSKeyboardExtension/KeyboardLayout.swift
@@ -29,24 +29,7 @@ struct KeyboardLayout {
         rows: [
             [ .q, .w, .e, .r, .t, .y, .u, .i,     .o,        .p,            ],
             [ .a, .s, .d, .f, .g, .h, .j, .k,     .l,        .apostrophe,   ],
-            [ .z, .x, .c, .v, .b, .n, .m, .comma, .fullStop, .questionMark, ]
-        ]
-    )
-    
-    static let shiftUpDictionary: [CharacterComponent: CharacterComponent] = [
-        .fullStop: .colon,
-        .comma: .semicolon,
-        .questionMark: .exclamationMark,
-        .apostrophe: .quotationMark,
-        .space: .lowLine
-    ]
-    
-    static let shiftDown = KeyboardLayout(
-        name: "ShiftDown",
-        rows: [
-            [ .ampersand,    .lessThanSign, .greaterThanSign, .verticalLine, .curlyBracket,  .solidus,  .seven, .eight, .nine,  .hyphenMinus, ],
-            [ .commercialAt, .numberSign,   .dollarSign,      .percentSign,  .parenthesis,   .asterisk, .four,  .five,  .six,   .plusSign,    ],
-            [ .graveAccent,  .tilde,        .caret,           .solidus,      .squareBracket, .zero,     .one,   .two,   .three, .equalsSign,  ]
+            [ .z, .x, .c, .v, .b, .n, .m, .comma, .fullStop, .questionMark, ],
         ]
     )
     
@@ -55,7 +38,24 @@ struct KeyboardLayout {
         rows: [
             [ .apostrophe,   .comma, .fullStop, .p, .y, .f, .g, .c, .r, .l, ],
             [ .a,            .o,     .e,        .u, .i, .d, .h, .t, .n, .s, ],
-            [ .questionMark, .q,     .j,        .k, .x, .b, .m, .w, .v, .z, ]
+            [ .questionMark, .q,     .j,        .k, .x, .b, .m, .w, .v, .z, ],
+        ]
+    )
+    
+    static let shiftUpDictionary: [CharacterComponent: CharacterComponent] = [
+        .fullStop: .colon,
+        .comma: .semicolon,
+        .questionMark: .exclamationMark,
+        .apostrophe: .quotationMark,
+        .space: .lowLine,
+    ]
+    
+    static let shiftDown = KeyboardLayout(
+        name: "ShiftDown",
+        rows: [
+            [ .ampersand,    .lessThanSign, .greaterThanSign, .verticalLine, .curlyBracket,  .solidus,  .seven, .eight, .nine,  .hyphenMinus, ],
+            [ .commercialAt, .numberSign,   .dollarSign,      .percentSign,  .parenthesis,   .asterisk, .four,  .five,  .six,   .plusSign,    ],
+            [ .graveAccent,  .tilde,        .caret,           .solidus,      .squareBracket, .zero,     .one,   .two,   .three, .equalsSign,  ],
         ]
     )
 }

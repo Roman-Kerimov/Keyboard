859075bbf6d9314f95b334237007f969179eaefb
Roman Kerimov <roma.kerimov@icloud.com>
2018-01-30 20:08:32 +0300

    Add macOSKeyboard target and macOSKeyboardTests target

---
+++ macOSKeyboard/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  macOSKeyboard
+//
+//  Created by Roman Kerimov on 2018-01-30.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOSKeyboard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOSKeyboard
+//
+//  Created by Roman Kerimov on 2018-01-30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

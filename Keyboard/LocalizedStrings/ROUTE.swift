3a589510641ae62d3973f55364602d4e2e74b642
Roman Kerimov <roma.kerimov@icloud.com>
2018-02-12 13:40:19 +0300

    Split file with localized strings

---
+++ Keyboard/LocalizedStrings/ROUTE.swift
@@ -0,0 +1,39 @@
+//
+//  ROUTE.swift
+//  Keyboard
+//
+//  Created by Roman Kerimov on 2018-02-12.
+//
+
+struct ROUTE: LocalizableString {
+    static var az:        String {return ""}
+    static var az_Cyrl:   String {return ""}
+    static var ca:        String {return ""}
+    static var cs:        String {return ""}
+    static var da:        String {return ""}
+    static var de:        String {return ""}
+    static var el:        String {return ""}
+    static var en:        String {return "Route"}
+    static var eo:        String {return ""}
+    static var es:        String {return ""}
+    static var es_MX:     String {return ""}
+    static var fi:        String {return ""}
+    static var fr:        String {return ""}
+    static var hr:        String {return ""}
+    static var hu:        String {return ""}
+    static var id:        String {return ""}
+    static var it:        String {return ""}
+    static var ms:        String {return ""}
+    static var nb:        String {return ""}
+    static var nl:        String {return ""}
+    static var pl:        String {return ""}
+    static var pt_PT:     String {return ""}
+    static var pt_BR:     String {return ""}
+    static var ro:        String {return ""}
+    static var ru:        String {return "Маршрут"}
+    static var sk:        String {return ""}
+    static var sv:        String {return ""}
+    static var tr:        String {return ""}
+    static var uk:        String {return ""}
+    static var vi:        String {return ""}
+}
---
+++ Keyboard/LocalizedStrings/SETTINGS.swift
@@ -0,0 +1,40 @@
+//
+//  SETTINGS.swift
+//  Keyboard
+//
+//  Created by Roman Kerimov on 2018-02-12.
+//
+
+struct SETTINGS: LocalizableString {
+    static var az:        String {return ""}
+    static var az_Cyrl:   String {return ""}
+    static var ca:        String {return "Configuració"}
+    static var cs:        String {return "Nastavení"}
+    static var da:        String {return "Indstillinger"}
+    static var de:        String {return "Einstellungen"}
+    static var el:        String {return "Ρυθμίσεις"}
+    static var en:        String {return "Settings"}
+    static var eo:        String {return "Agordoj"}
+    static var es:        String {return "Ajustes"}
+    static var es_MX:     String {return "Configuración"}
+    static var es_419:    String {return es_MX}
+    static var fi:        String {return "Asetukset"}
+    static var fr:        String {return "Réglages"}
+    static var hr:        String {return "Postavke"}
+    static var hu:        String {return "Beállítások"}
+    static var id:        String {return "Pengaturan"}
+    static var it:        String {return "Impostazioni"}
+    static var ms:        String {return "Seting"}
+    static var nb:        String {return "Innstillinger"}
+    static var nl:        String {return "Instellingen"}
+    static var pl:        String {return "Ustawienia"}
+    static var pt_PT:     String {return "Definições"}
+    static var pt_BR:     String {return es}
+    static var ro:        String {return "Configurări"}
+    static var ru:        String {return "Настройки"}
+    static var sk:        String {return "Nastavenia"}
+    static var sv:        String {return "Inställningar"}
+    static var tr:        String {return "Ayarlar"}
+    static var uk:        String {return "Параметри"}
+    static var vi:        String {return "Cài đặt"}
+}

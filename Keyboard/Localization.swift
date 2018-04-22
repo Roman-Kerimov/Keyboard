66c3a76c5cfa968c6a7db275fb7d28ced657987b
Roman Kerimov <roma.kerimov@icloud.com>
2018-04-23 01:39:27 +0300

    Refactor

--- Keyboard/Localization.swift
+++ Keyboard/Localization.swift
@@ -75,38 +75,36 @@ protocol LocalizedString {
 extension LocalizedString {
     static var string: String {
         switch Language.current {
-            
-        //case .az: return az
-        //case .az_Cyrl: return az_Cyrl
-        case .ca: return ca
-        case .cs: return cs
-        case .da: return da
-        case .de: return de
-        case .el: return el
-        case .en: return en
-        case .eo: return eo
-        case .es: return es
-        case .es_MX: return es_MX
-        case .fi: return fi
-        case .fr: return fr
-        case .hr: return hr
-        case .hu: return hu
-        case .id: return id
-        case .it: return it
-        case .ms: return ms
-        case .nb: return nb
-        case .nl: return nl
-        case .pl: return pl
-        case .pt_PT: return pt_PT
-        case .pt_BR: return pt_BR
-        case .ro: return ro
-        case .ru: return ru
-        case .sk: return sk
-        case .sv: return sv
-        case .tr: return tr
-        case .uk: return uk
-        case .vi: return vi
-            
+            //case .az: return az
+            //case .az_Cyrl: return az_Cyrl
+            case .ca: return ca
+            case .cs: return cs
+            case .da: return da
+            case .de: return de
+            case .el: return el
+            case .en: return en
+            case .eo: return eo
+            case .es: return es
+            case .es_MX: return es_MX
+            case .fi: return fi
+            case .fr: return fr
+            case .hr: return hr
+            case .hu: return hu
+            case .id: return id
+            case .it: return it
+            case .ms: return ms
+            case .nb: return nb
+            case .nl: return nl
+            case .pl: return pl
+            case .pt_PT: return pt_PT
+            case .pt_BR: return pt_BR
+            case .ro: return ro
+            case .ru: return ru
+            case .sk: return sk
+            case .sv: return sv
+            case .tr: return tr
+            case .uk: return uk
+            case .vi: return vi
         }
     }
 }

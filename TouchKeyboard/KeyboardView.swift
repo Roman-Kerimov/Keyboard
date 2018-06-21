02f1a4351593c454c6a1f39fdd89e065b3bd3e55
Roman Kerimov <roma.kerimov@icloud.com>
2018-06-22 01:57:37 +0300

    Refactor KeyboardView

--- TouchKeyboard/KeyboardView.swift
+++ TouchKeyboard/KeyboardView.swift
@@ -136,113 +136,6 @@ internal class KeyboardView: UIView {
         return keyViews
     }
     
-    private var scaleFactor: CGFloat {
-        return Bundle.main.isInterfaceBuilder ? 1 : bounds.width / UIScreen.main.bounds.width
-    }
-    
-    private var minimalScreenSize: CGSize {
-        return CGSize(width: 320 * scaleFactor, height: 480 * scaleFactor)
-    }
-    
-    private var  screenSize: CGSize {
-        return Bundle.main.isInterfaceBuilder ? bounds.size : UIScreen.main.bounds.size.applying(.init(scale: scaleFactor))
-    }
-    
-    private var coefficientOfIncreaseForMainButtons: CGFloat {
-        return layoutMode == .horizontal && isPrefferedVerticalMode ? 1.2 : 1
-    }
-    
-    private var deleteRowHeightInKeys: CGFloat {
-        return 1 / coefficientOfIncreaseForMainButtons
-    }
-    private var spaceRowHeightInKeys: CGFloat {
-        return deleteRowHeightInKeys
-    }
-    
-    private var horizontalIndentInKeys: CGFloat {
-        return layoutMode == .horizontal && isPrefferedVerticalMode ? 1 : 0.5
-    }
-    
-    private let maxKeyboardHeightRatio: CGFloat = 0.59
-    
-    private var maxKeyWidth: CGFloat {
-        return 1024/sizeInKeysForHorizontalMode.width * scaleFactor
-    }
-    private func maxKeyHeight(fromWidth width: CGFloat) -> CGFloat {
-        return width * 0.94
-    }
-    
-    private var sizeInKeys = CGSize()
-    
-    private var sizeInKeysForVerticalMode: CGSize {
-        return CGSize(
-            width: CGFloat(Keyboard.default.layout.columnCount / 2) + 0.2 + horizontalIndentInKeys*2,
-            height: deleteRowHeightInKeys + CGFloat(Keyboard.default.layout.rowCount * 2) + spaceRowHeightInKeys
-        )
-    }
-    
-    private var sizeInKeysForHorizontalMode: CGSize {
-        return CGSize(
-            width: CGFloat(Keyboard.default.layout.columnCount) + horizontalIndentInKeys*2,
-            height: deleteRowHeightInKeys + CGFloat(Keyboard.default.layout.rowCount) + spaceRowHeightInKeys
-        )
-    }
-    
-    private var keySize: CGSize {
-        let keyWidth = min(maxKeyWidth, screenSize.width / sizeInKeys.width)
-        
-        let maxKeyboardHeight = (screenSize.height * maxKeyboardHeightRatio)
-        
-        return CGSize(
-            width: keyWidth,
-            height: min(
-                max(
-                    min(
-                        maxKeyboardHeight / sizeInKeysForVerticalMode.height * coefficientOfIncreaseForMainButtons,
-                        maxKeyHeight(fromWidth: maxKeyWidth)
-                    ),
-                    maxKeyHeight(fromWidth: keyWidth)
-                ),
-                
-                maxKeyboardHeight / sizeInKeys.height
-            )
-        )
-    }
-    
-    private var labelFontSize: CGFloat {
-        return min(spaceRowHeight * 0.5, 36)
-    }
-    
-    private var halfKeyboardSize: CGSize {
-        return CGSize(
-            width: keySize.width * CGFloat(Keyboard.default.layout.columnCount / 2),
-            height: keySize.height * CGFloat(Keyboard.default.layout.rowCount)
-        )
-    }
-    
-    private var deleteRowHeight: CGFloat {
-        return deleteRowHeightInKeys * keySize.height
-    }
-    
-    private var layoutHeight: CGFloat {
-        return backgroundView.bounds.height - deleteRowHeight - spaceRowHeight
-    }
-    
-    private var spaceRowHeight: CGFloat {
-        return spaceRowHeightInKeys * keySize.height
-    }
-
-    private var horizontalIndent: CGFloat {
-        return horizontalIndentInKeys * keySize.width
-    }
-
-    private var size: CGSize {
-        return CGSize(
-            width: keySize.width * sizeInKeys.width,
-            height: keySize.height * sizeInKeys.height
-        )
-    }
-    
     private var heightConstraint: NSLayoutConstraint?
     
     private let backgroundView: UIView = .init()
@@ -253,10 +146,6 @@ internal class KeyboardView: UIView {
         colorScheme = darkColorScheme ? .dark : .default
         
         enterKey.isEnabled = false
-    }
-    
-    private var isPrefferedVerticalMode: Bool {
-        return bounds.width < self.minimalScreenSize.height
     }
     
     private let deleteRowView: DeleteRowView = .init()
@@ -285,6 +174,12 @@ internal class KeyboardView: UIView {
     
     override func layoutSubviews() {
         
+        let scaleFactor: CGFloat = Bundle.main.isInterfaceBuilder ? 1 : bounds.width / UIScreen.main.bounds.width
+        
+        let minimalScreenSize: CGSize = CGSize.init(width: 320, height: 480).applying(.init(scale: scaleFactor))
+        
+        let isPrefferedVerticalMode: Bool = bounds.width < minimalScreenSize.height
+        
         if layoutMode == .default {
             layoutMode = isPrefferedVerticalMode ? .vertical : .horizontal
         }
@@ -293,50 +188,108 @@ internal class KeyboardView: UIView {
             layoutMode = layoutMode == .vertical ? .horizontal : .vertical
         }
         
-        let isHorizontalMode = layoutMode == .horizontal || screenSize.height < self.minimalScreenSize.height
-        sizeInKeys = isHorizontalMode ? sizeInKeysForHorizontalMode : sizeInKeysForVerticalMode
+        let screenSize: CGSize = Bundle.main.isInterfaceBuilder ? bounds.size : UIScreen.main.bounds.size.applying(.init(scale: scaleFactor))
+        
+        let isHorizontalMode = layoutMode == .horizontal || screenSize.height < minimalScreenSize.height
+        
+        let horizontalIndentInKeys: CGFloat = layoutMode == .horizontal && isPrefferedVerticalMode ? 1 : 0.5
+        
+        let coefficientOfIncreaseForMainButtons: CGFloat = layoutMode == .horizontal && isPrefferedVerticalMode ? 1.2 : 1
+        
+        let deleteRowHeightInKeys: CGFloat = 1 / coefficientOfIncreaseForMainButtons
+        let spaceRowHeightInKeys: CGFloat = deleteRowHeightInKeys
+        
+        let sizeInKeysForVerticalMode: CGSize = .init(
+            width: CGFloat(Keyboard.default.layout.columnCount / 2) + 0.2 + horizontalIndentInKeys*2,
+            height: deleteRowHeightInKeys + CGFloat(Keyboard.default.layout.rowCount * 2) + spaceRowHeightInKeys
+        )
+        
+        let sizeInKeysForHorizontalMode: CGSize = .init(
+            width: CGFloat(Keyboard.default.layout.columnCount) + horizontalIndentInKeys*2,
+            height: deleteRowHeightInKeys + CGFloat(Keyboard.default.layout.rowCount) + spaceRowHeightInKeys
+        )
+        
+        let keyboardSizeInKeys = isHorizontalMode ? sizeInKeysForHorizontalMode : sizeInKeysForVerticalMode
+        
+        let maxKeyWidth: CGFloat = 1024/sizeInKeysForHorizontalMode.width * scaleFactor
+        
+        func maxKeyHeight(fromWidth width: CGFloat) -> CGFloat {
+            return width * 0.94
+        }
+        
+        let keyWidth = min(maxKeyWidth, screenSize.width / keyboardSizeInKeys.width)
+        
+        let maxKeyboardHeightRatio: CGFloat = 0.59
+        let maxKeyboardHeight = (screenSize.height * maxKeyboardHeightRatio)
+        
+        let keySize: CGSize = .init(
+            width: keyWidth,
+            height: min(
+                max(
+                    min(
+                        maxKeyboardHeight / sizeInKeysForVerticalMode.height * coefficientOfIncreaseForMainButtons,
+                        maxKeyHeight(fromWidth: maxKeyWidth)
+                    ),
+                    maxKeyHeight(fromWidth: keyWidth)
+                ),
+                
+                maxKeyboardHeight / keyboardSizeInKeys.height
+            )
+        )
+        
+        let keyboardSize: CGSize = .init(
+            width: keySize.width * keyboardSizeInKeys.width,
+            height: keySize.height * keyboardSizeInKeys.height
+        )
         
         if heightConstraint != nil {
-            heightConstraint?.constant = size.height
+            heightConstraint?.constant = keyboardSize.height
         }
         else {
-            heightConstraint = heightAnchor.constraint(equalToConstant: size.height)
+            heightConstraint = heightAnchor.constraint(equalToConstant: keyboardSize.height)
             heightConstraint?.priority = .defaultHigh
             heightConstraint?.isActive = true
         }
         
         if !Bundle.main.isInterfaceBuilder && !Bundle.main.isExtension {
             frame = UIScreen.main.bounds
-            frame.size.height = size.height
+            frame.size.height = keyboardSize.height
         }
         
         backgroundView.frame = .init(
             x: 0,
-            y: Bundle.main.isInterfaceBuilder ? bounds.height - size.height : 0,
+            y: Bundle.main.isInterfaceBuilder ? bounds.height - keyboardSize.height : 0,
             width: bounds.width,
-            height:  size.height
+            height:  keyboardSize.height
         )
         
-        let keySize = self.keySize
+        let deleteRowHeight: CGFloat = deleteRowHeightInKeys * keySize.height
+        let spaceRowHeight: CGFloat = spaceRowHeightInKeys * keySize.height
+        
         let keySpacing = deleteRowHeight * 0.1
-        let labelFontSize: CGFloat = self.labelFontSize
+        let labelFontSize: CGFloat = min(spaceRowHeight * 0.5, 36)
         
         deleteRowView.configure(
-            size: .init(width: size.width, height: deleteRowHeight),
+            size: .init(width: keyboardSize.width, height: deleteRowHeight),
             keySpacing: keySpacing, labelFontSize: labelFontSize
         )
         deleteRowView.frame.origin.y = 0
         
+        let layoutHeight: CGFloat = backgroundView.bounds.height - deleteRowHeight - spaceRowHeight
+        
         layoutView.configure(
-            size: .init(width: size.width, height: layoutHeight),
-            halfKeyboardSize: halfKeyboardSize,
+            size: .init(width: keyboardSize.width, height: layoutHeight),
+            halfKeyboardSize: .init(
+                width: keySize.width * CGFloat(Keyboard.default.layout.columnCount / 2),
+                height: keySize.height * CGFloat(Keyboard.default.layout.rowCount)
+            ),
             keySize: keySize, keySpacing: keySpacing, labelFontSize: labelFontSize,
-            horizontalIndent: horizontalIndent
+            horizontalIndent: horizontalIndentInKeys * keySize.width
         )
         layoutView.frame.origin.y = deleteRowHeight
         
         spaceRowView.configure(
-            size: .init(width: size.width, height: spaceRowHeight),
+            size: .init(width: keyboardSize.width, height: spaceRowHeight),
             keySpacing: keySpacing, labelFontSize: labelFontSize
         )
         spaceRowView.frame.origin.y = deleteRowHeight + layoutHeight
@@ -348,7 +301,7 @@ internal class KeyboardView: UIView {
             spaceRowView.center.x = backgroundView.bounds.midX
         }
         else {
-            let originX = backgroundView.bounds.maxX - size.width
+            let originX = backgroundView.bounds.maxX - keyboardSize.width
             deleteRowView.frame.origin.x = originX
             layoutView.frame.origin.x = originX
             spaceRowView.frame.origin.x = originX

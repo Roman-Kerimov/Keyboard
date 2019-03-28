56b59e974695a46f0c9b68ab802afa1303af3760
Roman Kerimov <roma.kerimov@icloud.com>
2019-03-29 01:11:30 +0300

    Add transformation from string to default shift gesture

--- Keyboard/String.swift
+++ Keyboard/String.swift
@@ -19,6 +19,19 @@ extension String {
     static let v: String = Character.v.description
     static let z: String = Character.z.description
     
+    var shiftGesture: String? {
+        
+        let decomposedUnicodeScalars = decomposedStringWithCanonicalMapping.unicodeScalars
+        
+        let shiftGestures = decomposedUnicodeScalars.compactMap {$0.description.characterComponents.shiftGestures.first}
+        
+        if shiftGestures.count != decomposedUnicodeScalars.count {
+            return nil
+        }
+        
+        return shiftGestures.joined()
+    }
+    
     var characterComponents: [CharacterComponent] {
         return characterComponentsDictionary[self]?.normalized ?? .init()
     }
---
+++ KeyboardTests/StringTests.swift
@@ -0,0 +1,17 @@
+//
+//  StringTests.swift
+//  Keyboard
+//
+//  Created by Roman Kerimov on 2019-03-28.
+//
+
+import XCTest
+@testable import Keyboard
+
+class StringTests: XCTestCase {
+
+    func testShiftGestures() {
+        XCTAssertEqual("ç̌".shiftGesture, "cs↙︎v↖︎")
+        XCTAssertEqual("3²".shiftGesture, "32↗︎")
+    }
+}

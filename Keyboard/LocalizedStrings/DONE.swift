716f787ed04f1b956bf2d79f7a37b43789d4f026
Roman Kerimov <roma.kerimov@icloud.com>
2019-07-03 05:41:52 +0300

    Refactor localized strings

--- Keyboard/LocalizedStrings/DONE.swift
+++ Keyboard/LocalizedStrings/DONE.swift
@@ -6,34 +6,34 @@
 //
 
 struct DONE: LocalizedString {
-    static var az:        String {return ""}
-    static var az_Cyrl:   String {return ""}
-    static var ca:        String {return ""}
-    static var cs:        String {return ""}
-    static var da:        String {return ""}
-    static var de:        String {return ""}
-    static var el:        String {return ""}
-    static var en:        String {return "Done"}
-    static var eo:        String {return ""}
-    static var es:        String {return ""}
-    static var es_MX:     String {return ""}
-    static var fi:        String {return ""}
-    static var fr:        String {return ""}
-    static var hr:        String {return ""}
-    static var hu:        String {return ""}
-    static var id:        String {return ""}
-    static var it:        String {return ""}
-    static var ms:        String {return ""}
-    static var nb:        String {return ""}
-    static var nl:        String {return ""}
-    static var pl:        String {return ""}
-    static var pt_PT:     String {return ""}
-    static var pt_BR:     String {return ""}
-    static var ro:        String {return ""}
-    static var ru:        String {return "Готово"}
-    static var sk:        String {return ""}
-    static var sv:        String {return ""}
-    static var tr:        String {return ""}
-    static var uk:        String {return ""}
-    static var vi:        String {return ""}
+    static let az        = ""
+    static let az_Cyrl   = ""
+    static let ca        = ""
+    static let cs        = ""
+    static let da        = ""
+    static let de        = ""
+    static let el        = ""
+    static let en        = "Done"
+    static let eo        = ""
+    static let es        = ""
+    static let es_MX     = ""
+    static let fi        = ""
+    static let fr        = ""
+    static let hr        = ""
+    static let hu        = ""
+    static let id        = ""
+    static let it        = ""
+    static let ms        = ""
+    static let nb        = ""
+    static let nl        = ""
+    static let pl        = ""
+    static let pt_PT     = ""
+    static let pt_BR     = ""
+    static let ro        = ""
+    static let ru        = "Готово"
+    static let sk        = ""
+    static let sv        = ""
+    static let tr        = ""
+    static let uk        = ""
+    static let vi        = ""
 }
--- Keyboard/LocalizedStrings/KEYBOARD.swift
+++ Keyboard/LocalizedStrings/KEYBOARD.swift
@@ -6,34 +6,34 @@
 //
 
 struct KEYBOARD: LocalizedString {
-    static var az:        String {return ""}
-    static var az_Cyrl:   String {return ""}
-    static var ca:        String {return "Teclat"}
-    static var cs:        String {return "Klávesnice"}
-    static var da:        String {return de}
-    static var de:        String {return "Tastatur"}
-    static var el:        String {return "Πληκτρολόγιo"}
-    static var en:        String {return "Keyboard "}
-    static var eo:        String {return "Klavaro"}
-    static var es:        String {return "Teclado"}
-    static var es_MX:     String {return es}
-    static var fi:        String {return "Näppäimistöt"}
-    static var fr:        String {return "Clavier"}
-    static var hr:        String {return "Tipkovnica"}
-    static var hu:        String {return "Billentyűyet"}
-    static var id:        String {return "Papan Ketik"}
-    static var it:        String {return "Tastiera"}
-    static var ms:        String {return "Papan Kekunci"}
-    static var nb:        String {return de}
-    static var nl:        String {return "Toetsenbord"}
-    static var pl:        String {return "Klawiatura"}
-    static var pt_PT:     String {return es}
-    static var pt_BR:     String {return es}
-    static var ro:        String {return "Tastatură"}
-    static var ru:        String {return "Клавиатура"}
-    static var sk:        String {return "Klávesnica"}
-    static var sv:        String {return "Tangentbord"}
-    static var tr:        String {return "Klavye"}
-    static var uk:        String {return "Клавіатура"}
-    static var vi:        String {return "Bàn phím"}
+    static let az        = ""
+    static let az_Cyrl   = ""
+    static let ca        = "Teclat"
+    static let cs        = "Klávesnice"
+    static let da        = de
+    static let de        = "Tastatur"
+    static let el        = "Πληκτρολόγιo"
+    static let en        = "Keyboard "
+    static let eo        = "Klaleto"
+    static let es        = "Teclado"
+    static let es_MX     = es
+    static let fi        = "Näppäimistöt"
+    static let fr        = "Clavier"
+    static let hr        = "Tipkovnica"
+    static let hu        = "Billentyűyet"
+    static let id        = "Papan Ketik"
+    static let it        = "Tastiera"
+    static let ms        = "Papan Kekunci"
+    static let nb        = de
+    static let nl        = "Toetsenbord"
+    static let pl        = "Klawiatura"
+    static let pt_PT     = es
+    static let pt_BR     = es
+    static let ro        = "Tastatură"
+    static let ru        = "Клавиатура"
+    static let sk        = "Klávesnica"
+    static let sv        = "Tangentbord"
+    static let tr        = "Klavye"
+    static let uk        = "Клавіатура"
+    static let vi        = "Bàn phím"
 }

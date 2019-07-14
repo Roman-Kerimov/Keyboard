ff85446850a1c5c01dc66b8be5a212783a89d8c7
Roman Kerimov <roma.kerimov@icloud.com>
2019-07-14 21:22:13 +0300

    Add LegalNoticesWindow class

--- Keyboard macOS/Menu/LegalNoticesMenuItem.swift
+++ Keyboard macOS/Menu/LegalNoticesMenuItem.swift
@@ -9,37 +9,15 @@ import SwiftUI
 
 class LegalNoticesMenuItem: LocalizedMenuItem {
     
-    let legalNoticesView: NSHostingView = .init(rootView: LegalNoticesView())
-    
     override func updateLocalizedStrings() {
         super.updateLocalizedStrings()
         
         title = LEGAL_NOTICES.string
-        window.title = LEGAL_NOTICES.string
     }
     
-    var window: GuideWindow = .init()
-    
-    override init() {
-        super.init()
-        
-        window.setContentSize(.init(width: 480, height: 500))
-        window.contentMaxSize.width = window.frame.size.width
-        window.contentMinSize.width = window.frame.size.width
-        
-        window.styleMask = [.resizable, .closable, .titled]
-        window.isReleasedWhenClosed = false
-        
-        window.contentView = legalNoticesView
-        window.backgroundColor = .textBackgroundColor
-        window.center()
-    }
-    
-    required init(coder decoder: NSCoder) {
-        super.init(coder: decoder)
-    }
+    var legalNoticesWindow: LegalNoticesWindow = .init()
     
     override func menuItemAction() {
-        window.orderFrontRegardless()
+        legalNoticesWindow.orderFrontRegardless()
     }
 }
--- Keyboard macOS/Windows/GuideWindow.swift
+++ Keyboard macOS/Windows/GuideWindow.swift
@@ -9,10 +9,19 @@ import AppKit
 
 class GuideWindow: NSWindow {
 
-    override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
-        super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
+    init() {
+        super.init(
+            contentRect: .zero,
+            styleMask: [.resizable, .closable, .titled],
+            backing: .buffered,
+            defer: true
+        )
+        
+        isReleasedWhenClosed = false
         
         delegate = self
+        
+        NotificationCenter.default.addLocaleObserver(self)
     }
     
 }
@@ -21,6 +30,9 @@ class GuideWindow: NSWindow {
 extension GuideWindow: NSWindowDelegate {
     
     func windowDidChangeOcclusionState(_ notification: Notification) {
+        
+        updateLocalizedStrings()
+        
         var psn: ProcessSerialNumber = .init(highLongOfPSN: 0, lowLongOfPSN: .init(kCurrentProcess))
         TransformProcessType(&psn, .init(NSApplication.shared.windows.contains(where: {$0 is GuideWindow && $0.isVisible}) ? kProcessTransformToForegroundApplication : kProcessTransformToUIElementApplication))
     }

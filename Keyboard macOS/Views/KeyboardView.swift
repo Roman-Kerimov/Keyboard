680b8a841afc8600dab4e41d36748201ad6c608e
Roman Kerimov <roma.kerimov@icloud.com>
2019-07-14 13:02:33 +0300

    Set option layout for preview of keyboard view

--- Keyboard macOS/Views/KeyboardView.swift
+++ Keyboard macOS/Views/KeyboardView.swift
@@ -29,6 +29,7 @@ struct KeyboardView : View {
 struct KeyboardView_Previews : PreviewProvider {
     static var previews: some View {
         Keyboard.default.delegate?.prepareForPreview()
+        Keyboard.default.previewLayout = .option
         
         return KeyboardView()
             .environmentObject(Keyboard.default)
